a93ba9f9b77ab601d81de216d120b785d11de865
jpeeck <jonas.peeck@bild.de>
2017-06-27 08:25:44 +0200

    WIP: Simple example, subscriptions, changeTriggers work

--- examples/simple/src/index.js
+++ examples/simple/src/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { connect, subscription, calculation, changeTrigger, asyncChangeTrigger, Provider } from 'slim-redux-react';
-import { createSlimReduxStore } from 'slim-redux';
+import { createSlimReduxStore, subscription as slimReduxSubscription } from 'slim-redux';
 
-const store = createSlimReduxStore({ counter: 123 });
+const store      = createSlimReduxStore({ counter: 1 }),
+      counterLog = slimReduxSubscription('state.counter', counter => console.log(`New state.counter: ${counter}`));
 
 // stuff definitions
-const counter = subscription('state.counter');
+const counter  = subscription('state.counter'),
+      inc      = changeTrigger('INCREMENT_COUNTER', (value, counter) => counter + value, 'state.counter'),
+      dec      = changeTrigger('DECREMENT_COUNTER', (value, counter) => counter - value, 'state.counter'),
+      asyncInc = asyncChangeTrigger({ inc }, (value, ct) => setTimeout(() => ct.inc(value), 2000));
 
-const Counter = ({ counter }) => (
-    <div>Counter: {counter}</div>
+const Counter = ({ counter, inc, dec, asyncInc }) => (
+    <div>
+        <div>Counter: {counter}</div>
+        <div>
+            <button onClick={e => inc(1)}>+</button>
+            <button onClick={e => dec(1)}>-</button>
+        </div>
+        <div>
+            <button onClick={e => asyncInc(5)}>Increase by 5 in 2 sec.</button>
+        </div>
+    </div>
 );
 
-const CounterContainer = connect(Counter, { counter: subscription('state.counter') });
+const CounterContainer = connect(Counter, { counter, inc, dec, asyncInc });
 
 const App = () => (
     <Provider store={store}>
