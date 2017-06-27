4e887ec24a6676d16db95acb120ae2944f5173ac
jpeeck <jonas.peeck@bild.de>
2017-06-27 08:25:11 +0200

    asyncChangeTrigger() had wrong signature

--- src/asyncChangeTrigger.js
+++ src/asyncChangeTrigger.js
@@ -1,8 +1,8 @@
 import { ASYNC_CHANGE_TRIGGER } from './constants';
 import { asyncChangeTrigger as slimReduxAsyncChangeTrigger } from 'slim-redux';
 
-export function asyncChangeTrigger(actionType, reducer, focusSubString) {
-    const createAsyncChangeTrigger = () => slimReduxAsyncChangeTrigger(actionType, reducer, focusSubString);
+export function asyncChangeTrigger(changeTriggers, triggerFunction) {
+    const createAsyncChangeTrigger = () => slimReduxAsyncChangeTrigger(changeTriggers, triggerFunction);
     return {
         type            : ASYNC_CHANGE_TRIGGER,
         creatorFunction : createAsyncChangeTrigger, 
