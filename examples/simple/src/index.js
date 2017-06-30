d04c795611ffbdd4b024189b83efb65efdab73e0
Jonas <jonas.peeck@googlemail.com>
2017-07-01 01:10:47 +0200

    Updated example, added comments

--- examples/simple/src/index.js
+++ examples/simple/src/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { connect, subscription, calculation, changeTrigger, asyncChangeTrigger, Provider } from 'slim-redux-react';
-import { createSlimReduxStore, subscription as slimReduxSubscription, calculation as slimReduxCalculation } from 'slim-redux';
+import { createSlimReduxStore, connect, subscription, calculation, changeTrigger, asyncChangeTrigger, Provider } from 'slim-redux-react';
 
-const store      = createSlimReduxStore({ counter: 1, stuff: 'state' });
-      
-// Uncomment these to get a console output of what's going on!   
-// const stateLog   = slimReduxSubscription('state', state => console.log(`New state.counter: ${JSON.stringify(state, null, 2)}`)),
-//       stateCalc  = slimReduxCalculation(['state.counter'], counter => counter + 5, calcPlus => console.log(`Increased counter value: ${calcPlus}`));
+const store = createSlimReduxStore({ counter: 1, stuff: 'state' });
 
-// stuff definitions
+
+/*
+    subscription(subscriptionString)
+*/ 
 const counter      = subscription('state.counter'),
-      inc          = changeTrigger('INCREMENT_COUNTER', (value, counter) => counter + value, 'state.counter'),
-      dec          = changeTrigger('DECREMENT_COUNTER', (value, counter) => counter - value, 'state.counter'),
-      asyncInc     = asyncChangeTrigger({ inc }, (value, ct) => {
-          setTimeout(() => ct.inc(value), 2000);
-      }),
-      counterPlus  = calculation(['state.counter'], counter => counter + 5);
+      currentState = subscription('state');
 
-const Counter = ({ counter, inc, dec, asyncInc, counterPlus }) => (
+/*
+    changeTrigger(actionType, reducerFunction [, focusString])
+*/
+const inc = changeTrigger('INCREMENT_COUNTER', (value, counter) => counter + value, 'state.counter'),
+      dec = changeTrigger('DECREMENT_COUNTER', (value, state) => ({ ...state, counter: state.counter - value }) );
+
+/*
+    asyncChangeTrigger(changeTriggers, changeTriggerFunction)
+*/
+const asyncInc = asyncChangeTrigger({ inc }, (value, ct) => {
+    setTimeout(() => ct.inc(value), 2000);
+});
+
+/*
+    calculation(subscriptions, calculationFunction)
+*/
+const counterPlus = calculation(['state.counter'], counter => counter + 5);
+
+
+// Visual component
+const Counter = ({ counter, inc, dec, asyncInc, counterPlus, currentState }) => (
     <div>
         <div>Counter (default): {counter}</div>
         <div>Counter (plus five): {counterPlus}</div>
@@ -30,15 +43,25 @@ const Counter = ({ counter, inc, dec, asyncInc, counterPlus }) => (
         <div>
             <button onClick={e => asyncInc(5)}>Increase by 5 in 2 sec.</button>
         </div>
+        <div>
+            <div>Current State:</div>
+            <div>{JSON.stringify(currentState, null, 2)}</div>
+        </div>
     </div>
 );
 
-const CounterContainer = connect(Counter, { counter, inc, dec, asyncInc, counterPlus });
 
+// Connect all the goodness to the visual react component, see how simple this is?
+const CounterContainer = connect(Counter, { counter, inc, dec, asyncInc, counterPlus, currentState });
+
+
+// Setup the store, just like you're used to from react-redux - with a <Provider/> component....
 const App = () => (
     <Provider store={store}>
         <CounterContainer/>
     </Provider>
 );
 
+
+// Render it out....you know the drill :)
 ReactDOM.render(<App />, document.getElementById('root'));
